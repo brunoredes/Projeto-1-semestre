4d190ef30ba64746c27ddce49e6ae14fe99e99d2
SorEduard <ceo.paludetto@gmail.com>
2019-04-15 21:42:49 -0300

    feat: Added sqlConnectionPool

--- Database.js
+++ Database.js
@@ -1,29 +1,30 @@
-'use strict';
+"use strict";
 const config = require("./config").database;
-const isNull = require('./script').isNull;
-const sql = require('mssql');
+const isNull = require("./script").isNull;
+const sql = require("mssql");
 
 module.exports = {
-	query: async (queryString) => {
-        return new Promise((resolve, reject) => {
-            if(isNull(queryString)){
-                return reject({
-                    message: "queryString is required!",
-                });
-            }
-
-            console.log('Establishing connection to Database...')
-            try{
-                const pool = await sql.connect(config);
-                console.log('Connected to Database!');
-                const res = await pool.request().query(queryString);
-                console.log('Query succeded!');
-                return resolve(res);
-            }
-            catch(err){
-                console.log(`Error executing query`);
-                return reject(err);
-            }
+  query: async queryString => {
+    return new Promise(async (resolve, reject) => {
+      if (isNull(queryString)) {
+        return reject({
+          message: "queryString is required!"
         });
-    }
+      }
+
+      console.log("Establishing connection to Database...");
+      try {
+        const pool = await new sql.ConnectionPool(config).connect();
+        console.log("Connected to Database!");
+        const res = await pool.request().query(queryString);
+        await sql.close();
+        console.log("Query succeded!");
+        return resolve(res);
+      } catch (err) {
+        console.log(`Error executing query`);
+        await sql.close();
+        return reject(err);
+      }
+    });
+  }
 };
